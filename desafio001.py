2d465167f17c0587f8c1090954492f30b3f54b71
Enos Cainan <cainan.ccarvalho@gmail.com>
2022-01-02 10:37:45 -0300

    Initial commit

---
+++ desafio001.py
@@ -0,0 +1,7 @@
+#Crie um programa que escreva "Olá, Mundo!" na tela
+
+print('Olá, Mundo!')
+
+# Também pode ser executado assim:
+msg = 'Olá, Mundo!'
+print(msg)
---
+++ teste.py
@@ -0,0 +1,3 @@
+nomes = ['Enos', 'Ariel', 'Maiza', 'Neidilson', 'Nina']
+for i in range(len(nomes)):
+    print(nomes[i])
